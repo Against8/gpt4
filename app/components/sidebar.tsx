129368f11419ff0ad7d4e5d189e0841436937ae5
xingxing <a283821090@163.com>
2023-05-13 15:17:48 +0800

    尝试加广告

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -121,7 +121,7 @@ export function SideBar(props: { className?: string }) {
       <div className={styles["sidebar-header"]}>
         <div className={styles["sidebar-title"]}>ChatGPT Next</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+        个人店铺地址：<a href="https://aigaochao.xyz" target="_blank">aigaochao.xyz</a>欢迎来购买gpt
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
